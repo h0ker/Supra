83eddbe2b0612e5ee3171df592ace1ae454b4ab0
CarbideCowboy <z_series@tuta.io>
2024-06-19 21:43:48 -0500

    Version increment for Jitpack fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.carbidecowboy.supraexample"
         minSdk = 28
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- supra/build.gradle.kts
+++ supra/build.gradle.kts
@@ -73,7 +73,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.CarbideCowboy"
                 artifactId = "Supra"
-                version = "0.0.5"
+                version = "0.0.6"
             }
         }
     }
