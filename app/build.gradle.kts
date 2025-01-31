0078cf985587d6d4e12f01056b8b0003e4c006b0
CarbideCowboy <z_series@tuta.io>
2025-01-31 14:50:01 -0600

    Java version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,14 +33,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     packaging {
         resources {
