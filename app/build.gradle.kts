ec7d6a5426a64a8692d61321a324cb28085d10dd
CarbideCowboy <z_series@tuta.io>
2025-05-15 08:02:44 -0500

    version increment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hoker.supraexample"
         minSdk = 31
         targetSdk = 35
-        versionCode = 107
-        versionName = "0.1.7"
+        versionCode = 108
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- supra/build.gradle.kts
+++ supra/build.gradle.kts
@@ -71,7 +71,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.hoker"
                 artifactId = "Supra"
-                version = "0.1.7"
+                version = "0.1.8"
             }
         }
     }
