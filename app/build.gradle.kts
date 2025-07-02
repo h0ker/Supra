363c461245dc3928b2fe9908670c101671300600
CarbideCowboy <z_series@tuta.io>
2025-07-01 19:16:52 -0500

    Added optional dismiss event to the show scan indicator with custom content function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hoker.supraexample"
         minSdk = 31
         targetSdk = 36
-        versionCode = 122
-        versionName = "0.1.22"
+        versionCode = 123
+        versionName = "0.1.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
