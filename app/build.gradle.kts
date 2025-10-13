12a5a530aab8afc55a4fc595cfeaa6a584c6b29d
CarbideCowboy <z_series@tuta.io>
2025-10-13 12:30:37 -0500

    Testing different tertiary color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hoker.supraexample"
         minSdk = 31
         targetSdk = 36
-        versionCode = 128
-        versionName = "0.1.28"
+        versionCode = 129
+        versionName = "0.1.29"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
