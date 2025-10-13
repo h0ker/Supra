656066bdf8e7a9d06fff00acde9bc8b4923dd367
CarbideCowboy <z_series@tuta.io>
2025-10-13 18:20:36 -0500

    Increased blur radius to avoid unreadable text. Removed scan icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hoker.supraexample"
         minSdk = 31
         targetSdk = 36
-        versionCode = 131
-        versionName = "0.1.31"
+        versionCode = 132
+        versionName = "0.1.32"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
