f38c495f93e09daf07f799c76df7ab72ad05320d
CarbideCowboy <z_series@tuta.io>
2026-03-20 13:39:51 -0500

    Added UI Theme const

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hoker.supraexample"
         minSdk = 31
         targetSdk = 36
-        versionCode = 203
-        versionName = "0.2.3"
+        versionCode = 204
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- supra/src/main/java/com/hoker/supra/domain/Consts.kt
+++ supra/src/main/java/com/hoker/supra/domain/Consts.kt
@@ -4,4 +4,5 @@ object Consts {
     const val SUPRA_SHARED_PREF_KEY = "supra_shared_prefs"
     const val SETTINGS_AUDIO_ENABLED = "settings_audio_enabled"
     const val SETTINGS_VIBRATION_ENABLED = "settings_vibration_enabled"
+    const val SETTINGS_SELECTED_UI_THEME = "settings_selected_ui_theme"
 }
